ce04bcdf590b37bd14607660c21126ce0b085f46
aitva <arod.louis@gmail.com>
2017-07-31 16:32:47 +0200

    Add a script to create DB.

---
+++ ajax-chat.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS `ajax-chat`;
+CREATE DATABASE `ajax-chat` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+DROP USER 'ajax-chat-user'@'localhost';
+CREATE USER 'ajax-chat-user'@'localhost' IDENTIFIED BY 'We Love SQL API!';
+GRANT ALL PRIVILEGES ON `ajax-chat`.* TO 'ajax-chat-user'@'localhost';
+
+USE `ajax-chat`;
+
+CREATE TABLE `message` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `text` TEXT NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL
+);
